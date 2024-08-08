d33697a4121a783dfaefcae86e5817b6a01091a3
HiiWee <wpdlzhf159@gmail.com>
2024-08-08 20:44:19 +0900

    feat: skeleton domain
    - member, product

---
+++ src/test/java/com/wootecam/luckyvickyauction/LuckyVickyAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wootecam.luckyvickyauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckyVickyAuctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
