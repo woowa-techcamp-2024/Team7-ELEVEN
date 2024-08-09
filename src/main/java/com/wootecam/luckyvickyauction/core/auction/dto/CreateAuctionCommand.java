23be39c2639adfcec3409c92933f572cba7d7a37
Basak <chgy2131@naver.com>
2024-08-10 02:36:09 +0900

    refactor: CreateAuctionCommand 멤버변수 오타 수정 (variationDuration)

--- src/main/java/com/wootecam/luckyvickyauction/core/auction/dto/CreateAuctionCommand.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/auction/dto/CreateAuctionCommand.java
@@ -14,13 +14,13 @@ import java.time.ZonedDateTime;
  * @param maximumPurchaseLimitCount 최대 구매 제한 수량
  * @param auctionType               경매 유형 {@link AuctionType}
  * @param variationWidth            가격 변동 폭
- * @param varitationDuration        가격 변동 주기
+ * @param variationDuration        가격 변동 주기
  * @param startedAt                 경매 시작 시간
  * @param finishedAt                경매 종료 시간
  */
 public record CreateAuctionCommand(
-        Long sellerId, String productName, int originPrice, int stock, int maximumPurchaseLimitCount,
-        AuctionType auctionType, int variationWidth, Duration varitationDuration, ZonedDateTime startedAt,
-        ZonedDateTime finishedAt
+    Long sellerId, String productName, int originPrice, int stock, int maximumPurchaseLimitCount,
+    AuctionType auctionType, int variationWidth, Duration variationDuration, ZonedDateTime startedAt,
+    ZonedDateTime finishedAt
 ) {
 }
