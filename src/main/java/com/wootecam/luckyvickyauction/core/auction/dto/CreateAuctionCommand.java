c3763c9287b6ac336658428dd5a78e36a84788fc
HyeonSik Choi <chgy2131@naver.com>
2024-08-09 17:03:48 +0900

    feat: AuctionServiceTest 경매 성공 테스트 추가 - 경매가 성공적으로 생성되면 예외가 발생하지 않는다. - CreateAuctionCommand: 경매 생성 command 구현

--- src/main/java/com/wootecam/luckyvickyauction/core/auction/dto/CreateAuctionCommand.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/auction/dto/CreateAuctionCommand.java
@@ -1,4 +1,26 @@
 package com.wootecam.luckyvickyauction.core.auction.dto;
 
-public record CreateAuctionCommand() {
+import com.wootecam.luckyvickyauction.core.auction.domain.AuctionType;
+import java.time.Duration;
+import java.time.ZonedDateTime;
+
+/**
+ * 경매 생성을 위한 명령 객체
+ *
+ * @param sellerId                  판매자 ID
+ * @param productName               상품 이름
+ * @param originPrice               상품 원가
+ * @param stock                     재고 수량
+ * @param maximumPurchaseLimitCount 최대 구매 제한 수량
+ * @param auctionType               경매 유형 {@link AuctionType}
+ * @param variationWidth            가격 변동 폭
+ * @param varitationDuration        가격 변동 주기
+ * @param startedAt                 경매 시작 시간
+ * @param finishedAt                경매 종료 시간
+ */
+public record CreateAuctionCommand(
+        Long sellerId, String productName, int originPrice, int stock, int maximumPurchaseLimitCount,
+        AuctionType auctionType, int variationWidth, Duration varitationDuration, ZonedDateTime startedAt,
+        ZonedDateTime finishedAt
+) {
 }
--- src/test/java/com/wootecam/luckyvickyauction/core/auction/service/AuctionServiceTest.java
+++ src/test/java/com/wootecam/luckyvickyauction/core/auction/service/AuctionServiceTest.java
@@ -1,7 +1,42 @@
 package com.wootecam.luckyvickyauction.core.auction.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+import com.wootecam.luckyvickyauction.core.auction.domain.AuctionType;
+import com.wootecam.luckyvickyauction.core.auction.domain.ConstantAuctionType;
+import com.wootecam.luckyvickyauction.core.auction.dto.CreateAuctionCommand;
+import java.time.Duration;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class AuctionServiceTest {
+    private AuctionService auctionService = new AuctionService();
 
+    @Test
+    @DisplayName("경매가 성공적으로 생성되면 예외가 발생하지 않는다.")
+    void create_auction_success_case() {
+        // given
+        Long sellerId = 1L;  // 판매자 정보
+        String productName = "상품이름";
+        int originPrice = 10000;
+        int stock = 999999;  // 재고
+        int maximumPurchaseLimitCount = 10;
+        AuctionType auctionType = new ConstantAuctionType();
+
+        int variationWidth = 1000;
+        Duration varitationDuration = Duration.ofMinutes(1L);  // 변동 시간 단위
+
+        ZonedDateTime startedAt = ZonedDateTime.of(2024, 8, 9, 0, 0, 0, 0, ZoneId.of("Asia/Seoul"));
+        ZonedDateTime finishedAt = ZonedDateTime.of(2024, 8, 9, 1, 0, 0, 0, ZoneId.of("Asia/Seoul"));
+
+        CreateAuctionCommand command = new CreateAuctionCommand(
+                sellerId, productName, originPrice, stock, maximumPurchaseLimitCount, auctionType, variationWidth,
+                varitationDuration, startedAt, finishedAt
+        );
+
+        // expect
+        assertThatNoException().isThrownBy(() -> auctionService.createAuction(command));
+    }
 }
