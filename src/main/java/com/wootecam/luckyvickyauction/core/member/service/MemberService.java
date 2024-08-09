8ca9f2083d708d253ea20509ab60b994368ee174
minseok-oh <minseok.dev@gmail.com>
2024-08-09 15:53:45 +0900

    feat: 사용자 회원가입 및 로그인 통합 로직 구현 - 로그인을 했을 떄 중복되는 아이디가 아니면 바로 세션으로 로그인 할당

--- src/main/java/com/wootecam/luckyvickyauction/core/member/service/MemberService.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/member/service/MemberService.java
@@ -1,4 +1,19 @@
 package com.wootecam.luckyvickyauction.core.member.service;
 
+import com.wootecam.luckyvickyauction.core.member.domain.MemberRepository;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public void signIn(String signInId, HttpSession session) {
+        if (memberRepository.isExist(signInId)) {
+            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+        }
+
+        memberRepository.save(signInId);
+        session.setAttribute("signInId", signInId);
+    }
 }
