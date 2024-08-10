dfb45bdacba53b3ac8e807ffeac592b5844f038a
yudonggeun <ydong98@gmail.com>
2024-08-10 18:43:22 +0900

    refactor: CreateAuctionCommandTest에서 사용하지 않는 VariationWidth Arguments 제거

--- src/main/java/com/wootecam/luckyvickyauction/core/auction/dto/CreateAuctionCommand.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/auction/dto/CreateAuctionCommand.java
@@ -34,7 +34,6 @@ public record CreateAuctionCommand(
     boolean isShowStock
 ) {
     private static final String ERROR_MAX_PURCHASE_LIMIT = "최대 구매 수량 제한은 0보다 커야 합니다. 최대 구매 수량 제한: %d";
-    private static final String ERROR_VARIATION_WIDTH = "가격 변동폭은 0보다 커야 합니다. 가격 변동폭: %d";
     private static final String ERROR_VARIATION_DURATION = "변동 시간 단위는 0보다 커야 합니다. 변동 시간: %s";
     private static final String ERROR_AUCTION_TIME = "경매의 시작 시간은 종료 시간보다 이전이어야 합니다. 시작 시간: %s, 종료 시간: %s";
     private static final String ERROR_STOCK = "재고는 인당 구매 수량보다 작을 수 없습니다. 재고: %d, 인당 구매 수량: %d";
