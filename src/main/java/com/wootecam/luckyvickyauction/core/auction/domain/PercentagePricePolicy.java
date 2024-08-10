6e89d336b0acb19721fe2b516d139ca741859b91
yudonggeun <ydong98@gmail.com>
2024-08-10 18:29:33 +0900

    feat: Auction 검증 코드 추가
    ## 구현 요구사항
    - 경매의 할인율은 100% 보다 작아야한다.

--- src/main/java/com/wootecam/luckyvickyauction/core/auction/domain/PercentagePricePolicy.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/auction/domain/PercentagePricePolicy.java
@@ -1,8 +1,27 @@
 package com.wootecam.luckyvickyauction.core.auction.domain;
 
+import com.wootecam.luckyvickyauction.global.exception.BadRequestException;
+import com.wootecam.luckyvickyauction.global.exception.ErrorCode;
+import lombok.Getter;
+
+@Getter
 public class PercentagePricePolicy implements PricePolicy {
+
+    private final double discountRate;
+
+    public PercentagePricePolicy(double discountRate) {
+        this.discountRate = discountRate;
+    }
+
     @Override
     public void validate(long price) {
+        // todo 데이터 손실 가능성
+        long nextPrice = (long) (price * (100 - discountRate)) / 100;
 
+        if (nextPrice <= 0) {
+            throw new BadRequestException(
+                    String.format("상품 원가는 할인율을 적용한 가격보다 커야 합니다. 상품 원가: %d, 할인율: %f", price, discountRate),
+                    ErrorCode.A010);
+        }
     }
 }
