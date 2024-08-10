70780ea6aa5a54d9c10f66e012109e989cbd3e92
HiiWee <wpdlzhf159@gmail.com>
2024-08-10 17:34:08 +0900

    refactor: wrapper class to primitive type
    - 수량, 가격은 null이 들어가지 않음

--- src/main/java/com/wootecam/luckyvickyauction/core/auction/dto/AuctionInfo.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/auction/dto/AuctionInfo.java
@@ -4,5 +4,5 @@ package com.wootecam.luckyvickyauction.core.auction.dto;
  * @param price    현재 단일 상품의 가격
  * @param quantity 현재 경매에 구매할 수 있는 재고 개수
  */
-public record AuctionInfo(Long price, Long quantity) {
+public record AuctionInfo(long price, long quantity) {
 }
