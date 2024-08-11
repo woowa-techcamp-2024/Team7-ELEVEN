7356005b0cce9998e81ad1a44625adefcc47ff6d
HiiWee <wpdlzhf159@gmail.com>
2024-08-11 18:53:06 +0900

    fix: 입찰에 실패하면 BidHistory가 저장되지 않도록 변경
    - submitBid가 성공하면 true, 실패하면 false를 반환해 처리하도록 함

--- src/main/java/com/wootecam/luckyvickyauction/core/payment/service/PaymentService.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/payment/service/PaymentService.java
@@ -19,12 +19,8 @@ public class PaymentService {
     private final BidHistoryRepository bidHistoryRepository;
 
     /**
-     *   1. 구매자 확인 <br>
-     *   2. 구매자 포인트를 감소 <br>
-     *   3. 판매자에게 포인트 지급 <br>
-     *   4. 구매 요청 <br>
-     *     - 실패하면 -> 예외 발생 및 구매자와 판매자 포인트 롤백 <br>
-     *     - 성공하면 -> BidHistory 저장 및 구매자, 판매자 업데이트 적용
+     * 1. 구매자 확인 <br> 2. 구매자 포인트를 감소 <br> 3. 판매자에게 포인트 지급 <br> 4. 구매 요청 <br> - 실패하면 -> 예외 발생 및 구매자와 판매자 포인트 롤백 <br> -
+     * 성공하면 -> BidHistory 저장 및 구매자, 판매자 업데이트 적용
      */
     public void process(Member buyer, long price, long auctionId, long quantity) {
         if (!buyer.isBuyer()) {
@@ -35,19 +31,19 @@ public class PaymentService {
         buyer.usePoint(price * quantity);
         seller.chargePoint(price * quantity);
 
-        submitBid(price, auctionId, quantity, buyer, seller);
-
-        Member savedBuyer = memberRepository.save(buyer);
-        Member savedSeller = memberRepository.save(seller);
-        BidHistory bidHistory = BidHistory.builder()
-                .productName(auctionInfo.productName())
-                .price(price)
-                .quantity(quantity)
-                .bidStatus(BidStatus.BID)
-                .seller(savedSeller)
-                .buyer(savedBuyer)
-                .build();
-        bidHistoryRepository.save(bidHistory);
+        if (submitBid(price, auctionId, quantity, buyer, seller)) {
+            Member savedBuyer = memberRepository.save(buyer);
+            Member savedSeller = memberRepository.save(seller);
+            BidHistory bidHistory = BidHistory.builder()
+                    .productName(auctionInfo.productName())
+                    .price(price)
+                    .quantity(quantity)
+                    .bidStatus(BidStatus.BID)
+                    .seller(savedSeller)
+                    .buyer(savedBuyer)
+                    .build();
+            bidHistoryRepository.save(bidHistory);
+        }
     }
 
     private Member findMemberObject(Long id) {
@@ -55,12 +51,14 @@ public class PaymentService {
                 .orElseThrow(() -> new NotFoundException("사용자를 찾을 수 없습니다.", ErrorCode.M002));
     }
 
-    private void submitBid(long price, long auctionId, long quantity, Member buyer, Member seller) {
+    private boolean submitBid(long price, long auctionId, long quantity, Member buyer, Member seller) {
         try {
             auctionService.submitBid(auctionId, price, quantity);
+            return true;
         } catch (BadRequestException e) {
             buyer.chargePoint(price * quantity);
             seller.usePoint(price * quantity);
+            return false;
         }
     }
 }
