ece57dd832d1689200a6f405d65db04db3b677b7
yudonggeun <ydong98@gmail.com>
2024-08-12 18:11:34 +0900

    feat: PricePolicy 정적 팩토리 메서드
    - percentagePricePolicy 퍼샌트 가격 정책
    - constantPricePolicy 절대값 가격 정책

--- src/main/java/com/wootecam/luckyvickyauction/core/auction/domain/PricePolicy.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/auction/domain/PricePolicy.java
@@ -3,4 +3,12 @@ package com.wootecam.luckyvickyauction.core.auction.domain;
 public interface PricePolicy {
 
     void validate(long price);
+
+    static PricePolicy percentagePricePolicy(int discountRate) {
+        return new PercentagePricePolicy(discountRate);
+    }
+
+    static PricePolicy constantPricePolicy(int variationWidth) {
+        return new ConstantPricePolicy(variationWidth);
+    }
 }
