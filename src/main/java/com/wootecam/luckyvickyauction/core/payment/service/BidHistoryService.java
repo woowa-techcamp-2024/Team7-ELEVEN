8ce1c28fc65574f1db646b6907c147eb45b2c8d7
Basak <chgy2131@naver.com>
2024-08-13 21:21:53 +0900

    feat: 경매이력 단건조회 메서드 시그니처 정의
    - bidHistoryId를 통해 BidHistoryInfo를 조회한다.

--- src/main/java/com/wootecam/luckyvickyauction/core/payment/service/BidHistoryService.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/payment/service/BidHistoryService.java
@@ -1,5 +1,18 @@
 package com.wootecam.luckyvickyauction.core.payment.service;
 
+import com.wootecam.luckyvickyauction.core.payment.domain.BidHistoryRepository;
+import com.wootecam.luckyvickyauction.core.payment.dto.BidHistoryInfo;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class BidHistoryService {
 
+    private final BidHistoryRepository bidHistoryRepository;
+
+    public BidHistoryInfo getBidHistoryInfo(long bidHistoryId) {
+        // 검증
+
+        // 반환
+        return null;
+    }
 }
