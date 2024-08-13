c351967c14e5e361041195c8a34a5a050fead5a5
Basak <chgy2131@naver.com>
2024-08-13 21:17:09 +0900

    test: BidHistoryServiceTest 추가

---
+++ src/test/java/com/wootecam/luckyvickyauction/core/payment/service/BidHistoryServiceTest.java
@@ -0,0 +1,37 @@
+package com.wootecam.luckyvickyauction.core.payment.service;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class BidHistoryServiceTest {
+
+    @Nested
+    class getBidHistory_메서드는 {
+
+        @Test
+        @Disabled
+        void 성공하는_경우_BidHistoryInfo를_반환받는다() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Test
+        @Disabled
+        void 존재하지않는_거래내역을_조회할때_예외가_발생한다() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Test
+        @Disabled
+        void 해당_거래내역의_소유자가_아닌경우_예외가_발생한다() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Test
+        @Disabled
+        void 잘못된_인자를_전달받는_경우_예외가_발생한다() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+}
