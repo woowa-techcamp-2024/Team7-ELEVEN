74ba0876afb91c904da82595ecdcf56c46ce3a1d
yudonggeun <ydong98@gmail.com>
2024-08-15 20:18:39 +0900

    refactor: ReceiptSelectCondition -> BuyerReceiptSearchCondition 네이밍 변경

--- src/main/java/com/wootecam/luckyvickyauction/core/payment/domain/BidHistoryRepository.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/payment/domain/BidHistoryRepository.java
@@ -1,6 +1,6 @@
 package com.wootecam.luckyvickyauction.core.payment.domain;
 
-import com.wootecam.luckyvickyauction.core.payment.dto.ReceiptSelectCondition;
+import com.wootecam.luckyvickyauction.core.payment.dto.BuyerReceiptSearchCondition;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,5 +10,5 @@ public interface BidHistoryRepository {
 
     Optional<BidHistory> findById(long bidHistoryId);
 
-    List<BidHistory> findAllBy(ReceiptSelectCondition condition);
+    List<BidHistory> findAllBy(BuyerReceiptSearchCondition condition);
 }
--- src/test/java/com/wootecam/luckyvickyauction/core/payment/dto/ReceiptSelectConditionTest.java
+++ src/test/java/com/wootecam/luckyvickyauction/core/payment/dto/ReceiptSelectConditionTest.java
@@ -17,7 +17,7 @@ class ReceiptSelectConditionTest {
     @ValueSource(ints = {0, 101})
     public void size가_1미만이거나_100초과인_경우_예외가_발생한다(int size) {
 
-        assertThatThrownBy(() -> new ReceiptSelectCondition(size))
+        assertThatThrownBy(() -> new BuyerReceiptSearchCondition(size))
                 .isInstanceOf(BadRequestException.class)
                 .hasFieldOrPropertyWithValue("errorCode", ErrorCode.G001);
     }
