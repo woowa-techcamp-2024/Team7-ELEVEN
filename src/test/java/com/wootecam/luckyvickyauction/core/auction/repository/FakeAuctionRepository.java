24939b2495f2241ab62982cf0d6059e92d633db0
yudonggeun <ydong98@gmail.com>
2024-08-15 16:32:16 +0900

    feat: 경매 목록 조회 repository interface(api) 정의
    - AuctionRepository#findAllBy 정의
    - FakeAuctionRepository 구현

--- src/test/java/com/wootecam/luckyvickyauction/core/auction/repository/FakeAuctionRepository.java
+++ src/test/java/com/wootecam/luckyvickyauction/core/auction/repository/FakeAuctionRepository.java
@@ -1,8 +1,10 @@
 package com.wootecam.luckyvickyauction.core.auction.repository;
 
 import com.wootecam.luckyvickyauction.core.auction.domain.Auction;
+import com.wootecam.luckyvickyauction.core.auction.dto.AuctionSearchCondition;
 import com.wootecam.luckyvickyauction.core.auction.infra.AuctionRepository;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
@@ -70,5 +72,14 @@ public class FakeAuctionRepository implements AuctionRepository {
     @Override
     public void deleteById(long id) {
         auctions.remove(id);
+    }
+
+
+    // TODO: [AuctionSearchCondition 조건 이후 변경 사항] [writeAt: 2024/08/15/16:30] [writeBy: yudonggeun]
+    @Override
+    public List<Auction> findAllBy(AuctionSearchCondition condition) {
+        return auctions.values().stream()
+                .filter(auction -> true)
+                .toList();
     }
 }
