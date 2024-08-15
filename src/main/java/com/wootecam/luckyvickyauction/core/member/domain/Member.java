728651b696646aabcd9ecfc369f7457d31229659
MinSeok Oh <minseok.dev@gmail.com>
2024-08-15 20:23:40 +0900

    feat: 회원가입 시 비밀번호 제약조건 코드 구현
    - 사용자 도메인에 검증 메서드를 구현했습니다

--- src/main/java/com/wootecam/luckyvickyauction/core/member/domain/Member.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/member/domain/Member.java
@@ -1,5 +1,7 @@
 package com.wootecam.luckyvickyauction.core.member.domain;
 
+import com.wootecam.luckyvickyauction.global.exception.BadRequestException;
+import com.wootecam.luckyvickyauction.global.exception.ErrorCode;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -19,6 +21,29 @@ public class Member {
         this.password = password;
         this.role = role;
         this.point = point;
+
+        validatePassword(password);
+    }
+
+    /**
+     * 비밀번호 유효성 검사 <br>
+     * 1. 비밀번호는 최소 8자, 최대 20자리이다. <br>
+     * 2. 비밀번호는 숫자가 반드시 포함되어야한다. <br>
+     * 3. 비밀번호는 알파벳 소문자가 반드시 포함되어야한다. <br>
+     * @param password 비밀번호
+     */
+    private void validatePassword(String password) {
+        if (password == null || password.length() < 8 || password.length() > 20) {
+            throw new BadRequestException("비밀번호는 8자 이상 20자 이하로 입력해주세요.", ErrorCode.M006);
+        }
+
+        if (!password.matches(".*[0-9].*")) {
+            throw new BadRequestException("비밀번호는 숫자가 반드시 포함되어야 합니다.", ErrorCode.M007);
+        }
+
+        if (!password.matches(".*[a-z].*")) {
+            throw new BadRequestException("비밀번호는 알파벳 소문자가 반드시 포함되어야 합니다.", ErrorCode.M008);
+        }
     }
 
     public static Member createMemberWithRole(String signInId, String password, String userRole) {
