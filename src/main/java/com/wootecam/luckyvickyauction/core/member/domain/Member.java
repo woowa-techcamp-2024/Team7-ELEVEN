5c33585a908cc0a3f5e6d6d51957689f52788123
Basak <chgy2131@naver.com>
2024-08-15 20:23:34 +0900

    feat: 회원 생성 시, 아이디가 공백인지 길이가 정책에 맞는지 검증한다.

--- src/main/java/com/wootecam/luckyvickyauction/core/member/domain/Member.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/member/domain/Member.java
@@ -1,5 +1,7 @@
 package com.wootecam.luckyvickyauction.core.member.domain;
 
+import com.wootecam.luckyvickyauction.global.exception.BadRequestException;
+import com.wootecam.luckyvickyauction.global.exception.ErrorCode;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,13 +14,33 @@ public class Member {
     private Role role;
     protected Point point;
 
+    private static final int MINIMUM_ID_LENGTH = 2;
+    private static final int MAXIMUM_ID_LENGTH = 20;
+    private static final String ERROR_ID_IS_BLANK = "아이디는 빈칸 또는 공백일 수 없습니다.";
+    private static final String ERROR_SIGN_IN_ID_LENGTH = "아이디 길이는 %d ~ %d자 여야합니다. 현재 길이: %d";
+
     @Builder
     public Member(Long id, String signInId, String password, Role role, Point point) {
+        validateSignInId(signInId);
+
         this.id = id;
         this.signInId = signInId;
         this.password = password;
         this.role = role;
         this.point = point;
+    }
+
+    private void validateSignInId(String signInId) {
+        // validate not blank
+        if (signInId == null || signInId.isBlank()) {
+            throw new BadRequestException(ERROR_ID_IS_BLANK, ErrorCode.M004);
+        }
+        // validate length
+        if (signInId.length() < MINIMUM_ID_LENGTH || MAXIMUM_ID_LENGTH < signInId.length()) {
+            throw new BadRequestException(
+                    String.format(ERROR_SIGN_IN_ID_LENGTH, MINIMUM_ID_LENGTH, MAXIMUM_ID_LENGTH, signInId.length()),
+                    ErrorCode.M005);
+        }
     }
 
     public static Member createMemberWithRole(String signInId, String password, String userRole) {
