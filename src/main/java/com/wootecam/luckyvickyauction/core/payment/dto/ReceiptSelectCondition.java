1bdb4ad873f50740cfdf6d05fdf38d07550cdb08
yudonggeun <ydong98@gmail.com>
2024-08-15 20:16:07 +0900

    feat: ReceiptSelectCondition 제약 구현
    조회하는 목록의 크기는 최대 100개까지 조회할 수 있습니다.

--- src/main/java/com/wootecam/luckyvickyauction/core/payment/dto/ReceiptSelectCondition.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/payment/dto/ReceiptSelectCondition.java
@@ -1,13 +1,23 @@
 package com.wootecam.luckyvickyauction.core.payment.dto;
 
-import lombok.Builder;
+import com.wootecam.luckyvickyauction.global.exception.BadRequestException;
+import com.wootecam.luckyvickyauction.global.exception.ErrorCode;
 
 /**
  * 거래 내역 목록 조회시 적용할 수 있는 조건을 나타내는 dto 입니다.
+ * @param size 조회할 거래 내역의 개수 (default: 10) (Min: 1, Max: 100)
  */
-public class ReceiptSelectCondition {
+public record ReceiptSelectCondition(
+        int size
+) {
 
-    @Builder
-    private ReceiptSelectCondition() {
+    public ReceiptSelectCondition {
+        pageSizeShouldBeBetweenOneAndHundred();
+    }
+
+    private void pageSizeShouldBeBetweenOneAndHundred() {
+        if (size < 1 || size > 100) {
+            throw new BadRequestException("size는 1 이상 100 이하의 값이어야 합니다.", ErrorCode.G001);
+        }
     }
 }
