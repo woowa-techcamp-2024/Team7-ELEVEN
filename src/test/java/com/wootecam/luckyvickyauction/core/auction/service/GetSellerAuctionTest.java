880a78401cc7f387fd99ba4d16e787d662417242
MinSeok Oh <minseok.dev@gmail.com>
2024-08-15 17:07:39 +0900

    test: 요청과 경매의 판매자 동일성 확인 테스트 코드 작성
    - 주석으로 #138 이슈를 연결했습니다

---
+++ src/test/java/com/wootecam/luckyvickyauction/core/auction/service/GetSellerAuctionTest.java
@@ -0,0 +1,83 @@
+package com.wootecam.luckyvickyauction.core.auction.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import com.wootecam.luckyvickyauction.core.auction.domain.Auction;
+import com.wootecam.luckyvickyauction.core.auction.dto.SellerAuctionInfo;
+import com.wootecam.luckyvickyauction.core.auction.fixture.AuctionFixture;
+import com.wootecam.luckyvickyauction.core.auction.infra.AuctionRepository;
+import com.wootecam.luckyvickyauction.core.auction.repository.FakeAuctionRepository;
+import com.wootecam.luckyvickyauction.core.member.domain.Member;
+import com.wootecam.luckyvickyauction.core.member.dto.SignInInfo;
+import com.wootecam.luckyvickyauction.core.member.fixture.MemberFixture;
+import com.wootecam.luckyvickyauction.global.exception.ErrorCode;
+import com.wootecam.luckyvickyauction.global.exception.UnauthorizedException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @see <a href="https://github.com/woowa-techcamp-2024/Team7-ELEVEN/issues/138">#138</a>
+ */
+abstract class GetSellerAuctionTest {
+
+    private AuctionRepository auctionRepository;
+    private AuctionService auctionService;
+
+    @BeforeEach
+    void setUp() {
+        auctionRepository = new FakeAuctionRepository();
+        auctionService = new AuctionService(auctionRepository);
+    }
+
+    @Nested
+    class 정상적인_요청이_오면 {
+
+        @Test
+        void 판매자의_경매목록을_반환한다() {
+            // given
+            Member seller = MemberFixture.createSellerWithDefaultPoint();
+            Auction auction = auctionRepository.save(AuctionFixture.createWaitingAuction());
+
+            SignInInfo signInInfo = new SignInInfo(seller.getId(), seller.getRole());
+
+            // when
+            SellerAuctionInfo sellerAuctionInfo = auctionService.getSellerAuction(signInInfo, auction.getId());
+
+            // then
+            assertAll(
+                    () -> assertThat(sellerAuctionInfo.auctionId()).isEqualTo(auction.getId()),
+                    () -> assertThat(sellerAuctionInfo.productName()).isEqualTo(auction.getProductName()),
+                    () -> assertThat(sellerAuctionInfo.originPrice()).isEqualTo(auction.getOriginPrice()),
+                    () -> assertThat(sellerAuctionInfo.currentPrice()).isEqualTo(auction.getCurrentPrice()),
+                    () -> assertThat(sellerAuctionInfo.originStock()).isEqualTo(auction.getOriginStock()),
+                    () -> assertThat(sellerAuctionInfo.currentStock()).isEqualTo(auction.getCurrentStock()),
+                    () -> assertThat(sellerAuctionInfo.maximumPurchaseLimitCount()).isEqualTo(
+                            auction.getMaximumPurchaseLimitCount()),
+                    () -> assertThat(sellerAuctionInfo.startedAt()).isEqualTo(auction.getStartedAt()),
+                    () -> assertThat(sellerAuctionInfo.finishedAt()).isEqualTo(auction.getFinishedAt()),
+                    () -> assertThat(sellerAuctionInfo.isShowStock()).isEqualTo(auction.isShowStock())
+            );
+        }
+    }
+
+    @Nested
+    class 요철한_판매자가_경매의_판매자가_아닌_경우 {
+
+        @Test
+        void 예외가_발생한다() {
+            // given
+            Member seller = MemberFixture.createSellerWithDefaultPoint();
+            Auction auction = auctionRepository.save(AuctionFixture.createWaitingAuction());
+            SignInInfo signInInfo = new SignInInfo(seller.getId() + 1, seller.getRole());
+
+            // expect
+            assertThatThrownBy(() -> auctionService.getSellerAuction(signInInfo, auction.getId()))
+                    .isInstanceOf(UnauthorizedException.class)
+                    .hasMessage("판매자는 자신이 등록한 경매만 조회할 수 있습니다.")
+                    .hasFieldOrPropertyWithValue("errorCode", ErrorCode.A027);
+        }
+    }
+}
