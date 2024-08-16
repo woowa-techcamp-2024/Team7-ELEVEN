5d6d0589d3bcdb6f316d7c31df3cffdd8d1150e3
minseok-oh <minseok.dev@gmail.com>
2024-08-16 16:56:18 +0900

    fix: 조회용 dto에 validation 제거

--- src/main/java/com/wootecam/luckyvickyauction/core/payment/dto/SellerReceiptSimpleInfo.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/payment/dto/SellerReceiptSimpleInfo.java
@@ -22,42 +22,4 @@ public record SellerReceiptSimpleInfo(
         long price,
         long quantity
 ) {
-
-    public static final String ERROR_PRODUCT_NAME = "상품 이름은 비어있을 수 없습니다.";
-    public static final String ERROR_PRICE = "거래 가격은 0보다 커야 합니다. 입찰 가격: %d";
-    public static final String ERROR_QUANTITY = "수량은 0보다 커야 합니다. 수량: %d";
-    public static final String ERROR_NULL_VALUE = "%s는 Null일 수 없습니다.";
-
-    public SellerReceiptSimpleInfo {
-        validateNotNull(type, "거래 타입");
-        validateNotNull(productName, "상품 이름");
-
-        validateProductName(productName);
-        validatePrice(price);
-        validateQuantity(quantity);
-    }
-
-    private void validateProductName(String productName) {
-        if (productName == null || productName.isBlank()) {
-            throw new BadRequestException(ERROR_PRODUCT_NAME, ErrorCode.B002);
-        }
-    }
-
-    private void validatePrice(long price) {
-        if (price <= 0) {
-            throw new BadRequestException(String.format(ERROR_PRICE, price), ErrorCode.B003);
-        }
-    }
-
-    private void validateQuantity(long quantity) {
-        if (quantity <= 0) {
-            throw new BadRequestException(String.format(ERROR_QUANTITY, quantity), ErrorCode.B004);
-        }
-    }
-
-    private void validateNotNull(Object value, String fieldName) {
-        if (value == null) {
-            throw new BadRequestException(String.format(ERROR_NULL_VALUE, fieldName), ErrorCode.G000);
-        }
-    }
 }
