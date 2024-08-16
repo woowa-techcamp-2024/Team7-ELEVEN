ab4418ef772794508fa130dacefd301fca87e95f
yudonggeun <ydong98@gmail.com>
2024-08-16 16:34:10 +0900

    feat: AuctionEntity 테이블 이름 추가

--- src/main/java/com/wootecam/luckyvickyauction/core/auction/entity/AuctionEntity.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/auction/entity/AuctionEntity.java
@@ -4,9 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 
-@Entity(name = "AUCTION")
+@Entity
+@Table(name = "AUCTION")
 @Getter
 public class AuctionEntity {
 
