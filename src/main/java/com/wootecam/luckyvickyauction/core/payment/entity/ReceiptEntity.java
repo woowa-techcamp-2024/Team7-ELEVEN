a3b5c519b9b826264ad6dcc0d84911565fc1c6bd
yudonggeun <ydong98@gmail.com>
2024-08-16 20:38:20 +0900

    test: ReceiptEntity 도메인 엔티티 변경 매퍼 테스트 작성
    - ReceiptEntity 생성자 생성
    - Mapper#convertToReceipt 테스트

--- src/main/java/com/wootecam/luckyvickyauction/core/payment/entity/ReceiptEntity.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/payment/entity/ReceiptEntity.java
@@ -1,14 +1,44 @@
 package com.wootecam.luckyvickyauction.core.payment.entity;
 
+import com.wootecam.luckyvickyauction.core.payment.domain.BidStatus;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.time.ZonedDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "RECEIPT")
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ReceiptEntity {
     @Id
     private Long id;
+    private String productName;
+    private long price;
+    private long quantity;
+    private BidStatus bidStatus;
+    private long auctionId;
+    private Long sellerId;
+    private Long buyerId;
+    private ZonedDateTime createdAt;
+    private ZonedDateTime updatedAt;
+
+    @Builder
+    private ReceiptEntity(Long id, String productName, long price, long quantity, BidStatus bidStatus, long auctionId,
+                          Long sellerId, Long buyerId, ZonedDateTime createdAt, ZonedDateTime updatedAt) {
+        this.id = id;
+        this.productName = productName;
+        this.price = price;
+        this.quantity = quantity;
+        this.bidStatus = bidStatus;
+        this.auctionId = auctionId;
+        this.sellerId = sellerId;
+        this.buyerId = buyerId;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
 }
