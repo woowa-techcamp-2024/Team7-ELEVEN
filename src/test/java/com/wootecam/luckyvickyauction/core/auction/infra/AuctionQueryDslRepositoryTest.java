302620cac152d89f17c2aeab16f573fb46838323
yudonggeun <ydong98@gmail.com>
2024-08-17 13:52:13 +0900

    test: 구매자가 경매목록 조회시 조회 개수만큼 경매목록을 조회한다.

---
+++ src/test/java/com/wootecam/luckyvickyauction/core/auction/infra/AuctionQueryDslRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.wootecam.luckyvickyauction.core.auction.infra;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.wootecam.luckyvickyauction.core.auction.dto.AuctionSearchCondition;
+import com.wootecam.luckyvickyauction.core.auction.entity.AuctionEntity;
+import java.util.List;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionQueryDslRepositoryTest {
+
+    @Autowired
+    AuctionJpaRepository repository;
+
+    @Nested
+    class 구매자_경매목록_조회_동적_쿼리_실행시 {
+
+        @Test
+        void 조회_개수만큼_경매목록을_조회한다() {
+
+            // given
+            int offset = 0;
+            int size = 3;
+            var condition = new AuctionSearchCondition(offset, size);
+
+            for (int i = 0; i < 10; i++) {
+                repository.save(AuctionEntity.builder()
+                        .build());
+            }
+
+            // when
+            List<AuctionEntity> result = repository.findAllBy(condition);
+
+            // then
+            assertThat(result.size()).isEqualTo(size);
+        }
+    }
+}
