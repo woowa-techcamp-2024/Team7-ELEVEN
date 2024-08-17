4f4d3341da103c756a47915442453c4804cce487
MinSeok Oh <minseok.dev@gmail.com>
2024-08-17 17:10:22 +0900

    fix: 컨버터 내의 예외를 커스텀 예외로 변경
    - DB나 ObjectMapper의 문제일 경우 InfrastructureException을 발생합니다

--- src/main/java/com/wootecam/luckyvickyauction/core/auction/entity/PricePolicyConverter.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/auction/entity/PricePolicyConverter.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wootecam.luckyvickyauction.core.auction.domain.ConstantPricePolicy;
 import com.wootecam.luckyvickyauction.core.auction.domain.PercentagePricePolicy;
 import com.wootecam.luckyvickyauction.core.auction.domain.PricePolicy;
+import com.wootecam.luckyvickyauction.global.exception.BadRequestException;
+import com.wootecam.luckyvickyauction.global.exception.ErrorCode;
+import com.wootecam.luckyvickyauction.global.exception.InfraStructureException;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 import java.io.IOException;
@@ -22,7 +25,7 @@ public class PricePolicyConverter implements AttributeConverter<PricePolicy, Str
         try {
             return objectMapper.writeValueAsString(PricePolicy.class);
         } catch (IOException e) {
-            throw new RuntimeException("해당 객체를 String으로 변환할 수 없습니다.", e);
+            throw new InfraStructureException("해당 객체를 String으로 변환할 수 없습니다.", ErrorCode.A030);
         }
     }
 
@@ -41,10 +44,10 @@ public class PricePolicyConverter implements AttributeConverter<PricePolicy, Str
                 case "CONSTANT":
                     return objectMapper.treeToValue(jsonNode, ConstantPricePolicy.class);
                 default:
-                    throw new IllegalArgumentException("해당 type으로 변환할 수 없습니다. 현재 type=" + type);
+                    throw new BadRequestException("해당 type으로 변환할 수 없습니다. 현재 type=" + type, ErrorCode.A031);
             }
         } catch (IOException e) {
-            throw new RuntimeException("해당 JSON을 PricePolicy 객체로 변환하는 데 실패했습니다.", e);
+            throw new InfraStructureException("해당 JSON을 PricePolicy 객체로 변환하는 데 실패했습니다.", ErrorCode.A032);
         }
     }
 }
