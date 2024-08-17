31f286916b57eb0fbfe128b21f794aafb9afa066
yudonggeun <ydong98@gmail.com>
2024-08-17 23:55:31 +0900

    feat: 판매자 경매 생성 API 구현
    - SellerAuctionController#createAuction 구현
    - SellerAuctionControllerTest 테스트 생성

--- src/main/java/com/wootecam/luckyvickyauction/core/auction/controller/SellerAuctionController.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/auction/controller/SellerAuctionController.java
@@ -3,8 +3,10 @@ package com.wootecam.luckyvickyauction.core.auction.controller;
 import com.wootecam.luckyvickyauction.core.auction.dto.CreateAuctionCommand;
 import com.wootecam.luckyvickyauction.core.auction.dto.SellerAuctionInfo;
 import com.wootecam.luckyvickyauction.core.auction.dto.SellerAuctionSimpleInfo;
+import com.wootecam.luckyvickyauction.core.auction.service.AuctionService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -13,17 +15,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-// @RestController  // TODO: [선행 @Repository가 생길 때, 주석을 풀 것] [writeAt: 2024/08/16/16:12] [writeBy: chhs2131]
+@RestController
 @RequestMapping("/auctions")
 @RequiredArgsConstructor
 public class SellerAuctionController {
 
+    private final AuctionService auctionService;
+
     // 판매자는 경매를 생성한다.
     @PostMapping
-    public void createAuction(@RequestBody CreateAuctionCommand command) {
-        // TODO: [Task에 맞게 로직 구현할 것!] [writeAt: 2024/08/16/17:40] [writeBy: chhs2131]
-        throw new UnsupportedOperationException();
+    public ResponseEntity<Void> createAuction(@RequestBody CreateAuctionCommand command) {
+        auctionService.createAuction(command);
+        return ResponseEntity.ok().build();
     }
 
     // 판매자는 경매를 취소한다.
---
+++ src/test/java/com/wootecam/luckyvickyauction/core/auction/controller/SellerAuctionControllerTest.java
@@ -0,0 +1,99 @@
+package com.wootecam.luckyvickyauction.core.auction.controller;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wootecam.luckyvickyauction.context.SpringBootTest;
+import com.wootecam.luckyvickyauction.core.auction.domain.PricePolicy;
+import com.wootecam.luckyvickyauction.core.auction.dto.CreateAuctionCommand;
+import com.wootecam.luckyvickyauction.core.auction.fixture.CreateAuctionCommandFixture;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+class SellerAuctionControllerTest extends SpringBootTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Nested
+    class 판매자_경매_등록_API {
+
+        @Test
+        void ConstantPolicy_경매_생성() throws Exception {
+            // given
+            CreateAuctionCommand condition = CreateAuctionCommandFixture.create()
+                    .pricePolicy(PricePolicy.createConstantPricePolicy(100))
+                    .build();
+
+            // expect
+            docsGiven
+                    .accept(MediaType.APPLICATION_JSON_VALUE)
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .body(objectMapper.writeValueAsString(condition))
+                    .when().post("/auctions")
+                    .then().log().all()
+                    .apply(document("auctions/create",
+                            requestFields(
+                                    fieldWithPath("sellerId").description("판매자 ID"),
+                                    fieldWithPath("productName").description("상품 이름"),
+                                    fieldWithPath("originPrice").description("상품 원가"),
+                                    fieldWithPath("stock").description("상품 재고"),
+                                    fieldWithPath("maximumPurchaseLimitCount").description("최대 구매 제한 수량"),
+                                    fieldWithPath("pricePolicy").description("가격 정책"),
+                                    fieldWithPath("pricePolicy.type").description("가격 정책 타입"),
+                                    fieldWithPath("pricePolicy.variationWidth").description("절대 가격 정책시 가격 절대 변동폭"),
+                                    fieldWithPath("variationDuration").description("경매 기간"),
+                                    fieldWithPath("isShowStock").description("재고 노출 여부"),
+                                    fieldWithPath("requestTime").description("요청 시간"),
+                                    fieldWithPath("startedAt").description("경매 시작 시간"),
+                                    fieldWithPath("finishedAt").description("경매 종료 시간"),
+                                    fieldWithPath("isShowStock").description("재고 노출 여부")
+                            )
+                    ))
+                    .statusCode(200);
+        }
+
+        @Test
+        void PercentagePolicy_경매_생성() throws Exception {
+            // given
+            CreateAuctionCommand condition = CreateAuctionCommandFixture.create()
+                    .pricePolicy(PricePolicy.createPercentagePricePolicy(10))
+                    .build();
+
+            // expect
+            docsGiven
+                    .accept(MediaType.APPLICATION_JSON_VALUE)
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .body(objectMapper.writeValueAsString(condition))
+                    .when().post("/auctions")
+                    .then().log().all()
+                    .apply(document("auctions/create",
+                            requestFields(
+                                    fieldWithPath("sellerId").description("판매자 ID"),
+                                    fieldWithPath("productName").description("상품 이름"),
+                                    fieldWithPath("originPrice").description("상품 원가"),
+                                    fieldWithPath("stock").description("상품 재고"),
+                                    fieldWithPath("maximumPurchaseLimitCount").description("최대 구매 제한 수량"),
+                                    fieldWithPath("pricePolicy").description("가격 정책"),
+                                    fieldWithPath("pricePolicy.type").description("가격 정책 타입"),
+                                    fieldWithPath("pricePolicy.discountRate").description("퍼센트 가격 정책시 가격 할인율"),
+                                    fieldWithPath("variationDuration").description("경매 기간"),
+                                    fieldWithPath("isShowStock").description("재고 노출 여부"),
+                                    fieldWithPath("requestTime").description("요청 시간"),
+                                    fieldWithPath("startedAt").description("경매 시작 시간"),
+                                    fieldWithPath("finishedAt").description("경매 종료 시간"),
+                                    fieldWithPath("isShowStock").description("재고 노출 여부")
+                            )
+                    ))
+                    .statusCode(200);
+        }
+    }
+}
