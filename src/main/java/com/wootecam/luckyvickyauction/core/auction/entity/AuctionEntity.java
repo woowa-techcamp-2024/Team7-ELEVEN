81fc2d58351228ab172ca64a1c9c0ec59e38c729
yudonggeun <ydong98@gmail.com>
2024-08-17 13:49:53 +0900

    feat: AuctionEntity 빌더 추가
    - 빌더 추가
    - jpa기능을 위한 기본 생성자 생성

--- src/main/java/com/wootecam/luckyvickyauction/core/auction/entity/AuctionEntity.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/auction/entity/AuctionEntity.java
@@ -5,14 +5,23 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "AUCTION")
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AuctionEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Builder
+    private AuctionEntity(Long id) {
+        this.id = id;
+    }
 }
