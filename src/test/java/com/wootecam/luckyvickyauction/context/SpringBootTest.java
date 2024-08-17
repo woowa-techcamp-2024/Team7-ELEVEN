8274c2c410feae3ab3374f67cf943162d9e61dcd
yudonggeun <ydong98@gmail.com>
2024-08-17 23:37:10 +0900

    feat: SpringBootTest 생성
    - 모든 SpringBoot 컨텍스트를 포함하는 테스트 환경을 나타내는 클래스를 생성하였습니다.

---
+++ src/test/java/com/wootecam/luckyvickyauction/context/SpringBootTest.java
@@ -0,0 +1,8 @@
+package com.wootecam.luckyvickyauction.context;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+@org.springframework.boot.test.context.SpringBootTest
+@AutoConfigureMockMvc
+public class SpringBootTest {
+}
