e5c2e02adafb3452a4922616467692f898fad158
Basak <chgy2131@naver.com>
2024-08-18 12:52:53 +0900

    feat: 판매자 전용 거래 이력 목록 조회 API EndPoint 추가

--- src/main/java/com/wootecam/luckyvickyauction/core/payment/controller/ReceiptController.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/payment/controller/ReceiptController.java
@@ -2,6 +2,8 @@ package com.wootecam.luckyvickyauction.core.payment.controller;
 
 import com.wootecam.luckyvickyauction.core.payment.dto.BuyerReceiptSearchCondition;
 import com.wootecam.luckyvickyauction.core.payment.dto.BuyerReceiptSimpleInfo;
+import com.wootecam.luckyvickyauction.core.payment.dto.SellerReceiptSearchCondition;
+import com.wootecam.luckyvickyauction.core.payment.dto.SellerReceiptSimpleInfo;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,11 +15,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class ReceiptController {
 
-    // 사용자는 자신의 거래 이력 목록을 조회할 수 있다.
+    // 구매자는 자신의 거래 이력 목록을 조회할 수 있다.
     @GetMapping
     public List<BuyerReceiptSimpleInfo> getReceipts(@RequestBody BuyerReceiptSearchCondition condition) {
         // TODO: [Task에 맞게 로직 구현할 것!] [writeAt: 2024/08/16/17:40] [writeBy: chhs2131]
         throw new UnsupportedOperationException();
     }
 
+    // 판매자는 자신의 거래 이력 목록을 조회할 수 있다.
+    @GetMapping("/seller")
+    public List<SellerReceiptSimpleInfo> getSellerReceipts(@RequestBody SellerReceiptSearchCondition condition) {
+        // TODO: [Task에 맞게 로직 구현할 것!] [writeAt: 2024/08/16/17:40] [writeBy: chhs2131]
+        throw new UnsupportedOperationException();
+    }
 }
