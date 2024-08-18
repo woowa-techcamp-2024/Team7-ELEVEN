44b69a657dea806008fe9b94b62bcb99f2154f4f
Basak <chgy2131@naver.com>
2024-08-18 13:25:41 +0900

    refactor: ReceiptSimpleInfo에 Builder를 추가한다.

--- src/main/java/com/wootecam/luckyvickyauction/core/payment/dto/BuyerReceiptSimpleInfo.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/payment/dto/BuyerReceiptSimpleInfo.java
@@ -1,6 +1,7 @@
 package com.wootecam.luckyvickyauction.core.payment.dto;
 
 import com.wootecam.luckyvickyauction.core.payment.domain.BidStatus;
+import lombok.Builder;
 
 /**
  * 구매자가 거래 내역 목록을 조회할 때, 노출되는 각 내역의 데이터를 나타내는 dto 입니다.
@@ -11,6 +12,7 @@ import com.wootecam.luckyvickyauction.core.payment.domain.BidStatus;
  * @param quantity  구매 수량
  * @param price     구매 가격
  */
+@Builder
 public record BuyerReceiptSimpleInfo(
         long id,
         BidStatus type,
