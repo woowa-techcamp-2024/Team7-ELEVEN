2195e75c05e2b7adf3947bf4ae8d074f3af00066
MinSeok Oh <minseok.dev@gmail.com>
2024-08-18 17:07:20 +0900

    feat: 거래이력 상세 조회 API 구현
    - 소유자 인증 부분은 제외하였습니다

--- src/main/java/com/wootecam/luckyvickyauction/core/payment/controller/ReceiptController.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/payment/controller/ReceiptController.java
@@ -1,17 +1,31 @@
 package com.wootecam.luckyvickyauction.core.payment.controller;
 
+import com.wootecam.luckyvickyauction.core.payment.dto.BidHistoryInfo;
 import com.wootecam.luckyvickyauction.core.payment.dto.BuyerReceiptSearchCondition;
 import com.wootecam.luckyvickyauction.core.payment.dto.BuyerReceiptSimpleInfo;
+import com.wootecam.luckyvickyauction.core.payment.service.BidHistoryService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// @RestController  // TODO: [선행 @Repository가 생길 때, 주석을 풀 것] [writeAt: 2024/08/16/16:12] [writeBy: chhs2131]
+@RestController
 @RequestMapping("/receipts")
 @RequiredArgsConstructor
 public class ReceiptController {
+
+    private final BidHistoryService bidHistoryService;
+
+    // 사용자는 자신의 거래 이력을 상세 조회할 수 있다.
+    @GetMapping("/{receiptId}")
+    public ResponseEntity<BidHistoryInfo> getReceipt(@PathVariable("receiptId") Long receiptId) {
+        BidHistoryInfo bidHistoryInfo = bidHistoryService.getBidHistoryInfo(receiptId);
+        return ResponseEntity.ok(bidHistoryInfo);
+    }
 
     // 사용자는 자신의 거래 이력 목록을 조회할 수 있다.
     @GetMapping
@@ -19,5 +33,4 @@ public class ReceiptController {
         // TODO: [Task에 맞게 로직 구현할 것!] [writeAt: 2024/08/16/17:40] [writeBy: chhs2131]
         throw new UnsupportedOperationException();
     }
-
 }
