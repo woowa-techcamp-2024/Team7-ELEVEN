06a1810ecf8080bd253e563c8e2484c618f6a11a
Basak <chgy2131@naver.com>
2024-08-18 13:29:05 +0900

    refactor: 구매자 거래 이력 조회에 '상품명'을 추가하고 필드 순서를 판매자 거래 이력 반환값과 동일하게 수정

--- src/main/java/com/wootecam/luckyvickyauction/core/payment/dto/BuyerReceiptSimpleInfo.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/payment/dto/BuyerReceiptSimpleInfo.java
@@ -6,17 +6,19 @@ import lombok.Builder;
 /**
  * 구매자가 거래 내역 목록을 조회할 때, 노출되는 각 내역의 데이터를 나타내는 dto 입니다.
  *
- * @param id        거내 내역 식별자입니다.
- * @param type      거래 타입 {@link BidStatus}
- * @param auctionId 구매한 경매의 식별자입니다.
- * @param quantity  구매 수량
- * @param price     구매 가격
+ * @param id          거내 내역 식별자입니다.
+ * @param auctionId   구매한 경매의 식별자입니다.
+ * @param type        거래 타입 {@link BidStatus}
+ * @param productName 상품명
+ * @param quantity    구매 수량
+ * @param price       구매 가격
  */
 @Builder
 public record BuyerReceiptSimpleInfo(
         long id,
-        BidStatus type,
         long auctionId,
+        BidStatus type,
+        String productName,
         long quantity,
         long price
 ) {
