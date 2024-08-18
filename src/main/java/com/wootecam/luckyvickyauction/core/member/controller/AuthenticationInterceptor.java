6f7acc8e46e47c444a231c030b6ca3f606331227
HiiWee <wpdlzhf159@gmail.com>
2024-08-18 15:26:26 +0900

    feat: 권한 검증을 위한 인터셉터를 생성한다

---
+++ src/main/java/com/wootecam/luckyvickyauction/core/member/controller/AuthenticationInterceptor.java
@@ -0,0 +1,11 @@
+package com.wootecam.luckyvickyauction.core.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationInterceptor implements HandlerInterceptor {
+
+}
