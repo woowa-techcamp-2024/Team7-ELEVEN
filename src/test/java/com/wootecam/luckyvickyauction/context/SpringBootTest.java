57eafee864b7d4cc38193237f9f641d2348358c2
유동근 <67232422+yudonggeun@users.noreply.github.com>
2024-08-18 14:48:53 +0900

    feat: 판매자는 경매를 REST API를 통해서 등록(생성)할 수 있다. (#214)
    * chore: h2 database 임시 설정
    
    - application.properties -> application.yaml 변경
    - h2 관련 설정 추가
    
    * feat: AuctionService 빈 등록
    
    - @Service 어노테이션 추가
    
    * feat: PricePolicy json 직렬화 어노테이션 추가
    
    - PricePolicy를 직렬화/역직렬화 할때 발생하는 오류를 해결하기 위해서 관련 설정을 추가하였습니다.
    
    * feat: CreateAuctionCommandFixture 생성
    
    - 테스트를 용이하게 하기위한 Fixture를 생성하였습니다.
    
    * feat: SpringBootTest 생성
    
    - 모든 SpringBoot 컨텍스트를 포함하는 테스트 환경을 나타내는 클래스를 생성하였습니다.
    
    * feat: JsonConfig 생성
    
    - PricePolicy를 직렬화하면 type을 제외한 다른 모든 값이 변환되지 않은 오류를 해결하기 위해서 ObjectMapper 관련 설정을 추가합니다.
    
    * feat: 판매자 경매 생성 API 구현
    
    - SellerAuctionController#createAuction 구현
    - SellerAuctionControllerTest 테스트 생성
    
    * docs: Github issue 문서 링크 추가
    
    * feat: pr 피드백 반영
    
    * fix: restdoc identifier 수정
    
    * fix: restdoc identifier 수정
    
    - /과 ' '를 혼용해서 사용할시에 테스트가 깨져서 변경하였습니다.
    
    ---------
    
    Co-authored-by: 이호석 <66772624+HiiWee@users.noreply.github.com>

--- src/test/java/com/wootecam/luckyvickyauction/context/SpringBootTest.java
+++ src/test/java/com/wootecam/luckyvickyauction/context/SpringBootTest.java
@@ -1,44 +1,27 @@
-package com.wootecam.luckyvickyauction.documentation;
+package com.wootecam.luckyvickyauction.context;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 
-import com.wootecam.luckyvickyauction.core.auction.controller.SellerAuctionController;
-import com.wootecam.luckyvickyauction.core.auction.service.AuctionService;
-import com.wootecam.luckyvickyauction.core.member.controller.AuthController;
-import com.wootecam.luckyvickyauction.core.member.service.MemberService;
-import com.wootecam.luckyvickyauction.documentation.errorcode.FakeErrorCodeController;
-import com.wootecam.luckyvickyauction.global.config.JsonConfig;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import io.restassured.module.mockmvc.specification.MockMvcRequestSpecification;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-@WebMvcTest({
-        FakeErrorCodeController.class,
-        AuthController.class,
-        SellerAuctionController.class
-})
-@Import(JsonConfig.class)
+@org.springframework.boot.test.context.SpringBootTest
+@AutoConfigureMockMvc
 @ExtendWith(RestDocumentationExtension.class)
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-public class DocumentationTest {
+public class SpringBootTest {
 
     protected MockMvcRequestSpecification docsGiven;
-
-    @MockBean
-    protected MemberService memberService;
-    @MockBean
-    protected AuctionService auctionService;
 
     @BeforeEach
     void setUp(final WebApplicationContext webApplicationContext,
