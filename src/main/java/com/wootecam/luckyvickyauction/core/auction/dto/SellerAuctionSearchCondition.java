e9884009ec31d16e2475eacf9b33c46c7ec68a48
HyeonSik Choi <chgy2131@naver.com>
2024-08-19 20:23:32 +0900

    feat: 경매 조회용 DTO 추가 및 수정
    - 판매자 경매 조회용 DTO를 추가한다.
    - 조회 항목 수(size)가 정책을 벗어나는 경우 G001 예외를 발생시킨다.

--- src/main/java/com/wootecam/luckyvickyauction/core/auction/dto/SellerAuctionSearchCondition.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/auction/dto/SellerAuctionSearchCondition.java
@@ -1,4 +1,19 @@
 package com.wootecam.luckyvickyauction.core.auction.dto;
 
-public record SellerAuctionSearchCondition() {
+import com.wootecam.luckyvickyauction.global.exception.BadRequestException;
+import com.wootecam.luckyvickyauction.global.exception.ErrorCode;
+
+public record SellerAuctionSearchCondition(
+        int offset,
+        int size
+) {
+    public SellerAuctionSearchCondition {
+        validateSizeBetween(1, 100, size);
+    }
+
+    private void validateSizeBetween(int from, int to, int size) {
+        if (size < from || size > to) {
+            throw new BadRequestException("size는 " + from + " 이상 " + to + " 이하의 값이어야 합니다.", ErrorCode.G001);
+        }
+    }
 }
