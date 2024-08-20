495e31e9c143f498d8210c492a7c8357dc76957b
HiiWee <wpdlzhf159@gmail.com>
2024-08-20 16:32:16 +0900

    refactor: 포인트 충전 API doc 정리 완료

--- src/test/java/com/wootecam/luckyvickyauction/documentation/PaymentDocument.java
+++ src/test/java/com/wootecam/luckyvickyauction/documentation/PaymentDocument.java
@@ -1,5 +1,6 @@
 package com.wootecam.luckyvickyauction.documentation;
 
+import static com.wootecam.luckyvickyauction.documentation.DocumentFormatGenerator.getConstraints;
 import static org.springframework.restdocs.cookies.CookieDocumentation.cookieWithName;
 import static org.springframework.restdocs.cookies.CookieDocumentation.requestCookies;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
@@ -20,11 +21,9 @@ class PaymentDocument extends DocumentationTest {
 
     @Test
     void 사용자는_포인트를_충전할_수_있다_API() throws Exception {
-        // given
-        var command = new BuyerChargePointCommand(10000);
+        BuyerChargePointCommand command = new BuyerChargePointCommand(10000);
         SignInInfo sellerInfo = new SignInInfo(1L, Role.BUYER);
 
-        // expect
         mockMvc.perform(post("/payments/points/charge")
                         .contentType(MediaType.APPLICATION_JSON_VALUE)
                         .accept(MediaType.APPLICATION_JSON_VALUE)
@@ -32,14 +31,15 @@ class PaymentDocument extends DocumentationTest {
                         .sessionAttr("signInMember", sellerInfo)
                         .content(objectMapper.writeValueAsString(command))
                 )
-                .andExpect(status().isOk())
-                .andDo(document("post payments/points/charge",
+                .andDo(document("payment/chargePoint/success",
                         requestCookies(
                                 cookieWithName("JSESSIONID").description("세션 ID")
                         ),
                         requestFields(
                                 fieldWithPath("amount").type(JsonFieldType.NUMBER).description("충전할 포인트 금액")
+                                        .attributes(getConstraints("constraints", "Long.MAX_VALUE까지 충전 가능"))
                         )
-                ));
+                ))
+                .andExpect(status().isOk());
     }
 }
