519ada8523815294c86ed3861b41b8d87f802667
이호석 <66772624+HiiWee@users.noreply.github.com>
2024-08-20 22:12:14 +0900

    feat: service에 트랜잭션 어노테이션 적용 (#257)

--- src/main/java/com/wootecam/luckyvickyauction/core/payment/service/PaymentService.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/payment/service/PaymentService.java
@@ -16,8 +16,10 @@ import com.wootecam.luckyvickyauction.global.exception.UnauthorizedException;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class PaymentService {
 
@@ -29,6 +31,7 @@ public class PaymentService {
      * 1. 구매자 확인 <br> 2. 구매자 포인트를 감소 <br> 3. 판매자에게 포인트 지급 <br> 4. 구매 요청 <br> - 실패하면 -> 예외 발생 및 구매자와 판매자 포인트 롤백 <br> -
      * 성공하면 -> Receipt 저장 및 구매자, 판매자 업데이트 적용
      */
+    @Transactional
     public void process(SignInInfo buyerInfo, long price, long auctionId, long quantity, LocalDateTime requestTime) {
         Member buyer = findMemberObject(buyerInfo.id());
         AuctionInfo auctionInfo = auctionService.getAuction(auctionId);
@@ -62,6 +65,7 @@ public class PaymentService {
      * @param buyerInfo 환불을 요청한 사용자
      * @param receiptId 환불할 거래 내역의 id
      */
+    @Transactional
     public void refund(SignInInfo buyerInfo, long receiptId) {
         if (!buyerInfo.isType(Role.BUYER)) {
             throw new UnauthorizedException("구매자만 환불을 할 수 있습니다.", ErrorCode.P000);
@@ -105,6 +109,7 @@ public class PaymentService {
      * @param memberInfo  포인트를 충전할 사용자
      * @param chargePoint 충전할 포인트
      */
+    @Transactional
     public void chargePoint(SignInInfo memberInfo, long chargePoint) {
         if (chargePoint < 0) {
             throw new BadRequestException("포인트는 음수가 될 수 없습니다. 충전 포인트=" + chargePoint, ErrorCode.P005);
--- src/main/java/com/wootecam/luckyvickyauction/core/payment/service/ReceiptService.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/payment/service/ReceiptService.java
@@ -15,8 +15,10 @@ import com.wootecam.luckyvickyauction.global.util.Mapper;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ReceiptService {
 
