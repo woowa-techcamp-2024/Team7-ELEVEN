5b3be869960c746b759968ef8e47a99fa96c5c0c
Basak <chgy2131@naver.com>
2024-08-22 02:02:40 +0900

    feat: RedissonClient Bean 등

--- src/main/java/com/wootecam/luckyvickyauction/global/config/RedisConfig.java
+++ src/main/java/com/wootecam/luckyvickyauction/global/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package com.wootecam.luckyvickyauction.global.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +23,15 @@ public class RedisConfig {
 
     @Value("${spring.data.redis.password}")
     private String redisPassword;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        RedissonClient redisson = null;
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://" + redisHost + ":" + redisPort);
+        redisson = Redisson.create(config);
+        return redisson;
+    }
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
