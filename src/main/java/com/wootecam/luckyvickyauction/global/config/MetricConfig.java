09fc223ce116f101a5bef2360e717e3e3a8eec21
유동근 <67232422+yudonggeun@users.noreply.github.com>
2024-08-22 13:43:19 +0900

    feat: 모니터링 환경 구성 (#235)
    * chore: spring boot actuator 의존성 추가
    
    * chore: actuator 메트릭 설정
    
    - prometheus 메트릭 엔드포인트 노출
    - tomcat mbean 추가 설정
    
    * feat: MetricConfig 추가
    
    - 메트릭 수집 설정 추가
    - https://grafana.com/grafana/dashboards/6756-spring-boot-statistics/
    
    * chore: prometheus 설정 파일 추가
    
    - 로컬에서 동작하는 설정으로 추가합니다.
    
    * chore: grafana 설정 파일 추가
    
    - prometheus 데이터 소스 추가 : prometheus.yml
    - dashboards.yml 대시보드 파일 설정 매핑 추가
    
    * chore: prometheus, grafana docker compose 추가
    
    * chore: spring boot metric port 변경
    
    - 8081에서 메트릭 정보를 수집합니다.
    
    ---------
    
    Co-authored-by: 이호석 <66772624+HiiWee@users.noreply.github.com>

---
+++ src/main/java/com/wootecam/luckyvickyauction/global/config/MetricConfig.java
@@ -0,0 +1,15 @@
+package com.wootecam.luckyvickyauction.global.config;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MetricConfig {
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config().commonTags("application", "lucky-vicky-auction");
+    }
+}
