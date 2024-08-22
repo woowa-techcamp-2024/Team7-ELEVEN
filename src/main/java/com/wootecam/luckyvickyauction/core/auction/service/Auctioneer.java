54c425536a21c8cbeaf504391ec93f12e38089ea
HyeonSik Choi <chgy2131@naver.com>
2024-08-22 10:56:38 +0900

    style: Auctioneer 관련 주석 추가

--- src/main/java/com/wootecam/luckyvickyauction/core/auction/service/Auctioneer.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/auction/service/Auctioneer.java
@@ -3,6 +3,10 @@ package com.wootecam.luckyvickyauction.core.auction.service;
 import com.wootecam.luckyvickyauction.core.member.dto.SignInInfo;
 import java.time.LocalDateTime;
 
+/**
+ * 경매 입찰 로직 분리
+ * <a href="{https://github.com/woowa-techcamp-2024/Team7-ELEVEN/issues/246}">#246</a>
+ */
 public interface Auctioneer {
 
     void process(SignInInfo buyerInfo, long price, long auctionId, long quantity, LocalDateTime requestTime);
