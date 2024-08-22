9f0ac2804c6874c07d3cc6b2671ab28f44721d40
HiiWee <wpdlzhf159@gmail.com>
2024-08-22 12:46:33 +0900

    feat: cors 설정 적용

--- src/main/java/com/wootecam/luckyvickyauction/global/config/WebConfig.java
+++ src/main/java/com/wootecam/luckyvickyauction/global/config/WebConfig.java
@@ -7,6 +7,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,6 +18,14 @@ public class WebConfig implements WebMvcConfigurer {
     private final AuthenticationInterceptor authenticationInterceptor;
     private final AuthenticationArgumentResolver authenticationArgumentResolver;
     private final CurrentTimeArgumentResolver currentTimeArgumentResolver;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "HEAD", "POST", "DELETE", "TRACE", "OPTIONS", "PATCH", "PUT");
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
