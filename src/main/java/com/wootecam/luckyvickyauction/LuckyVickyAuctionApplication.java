8eb8018afaad3fd405cdbc77a50b6b2e571341ec
HiiWee <wpdlzhf159@gmail.com>
2024-08-24 19:11:50 +0900

    remove: 출력문 삭제

--- src/main/java/com/wootecam/luckyvickyauction/LuckyVickyAuctionApplication.java
+++ src/main/java/com/wootecam/luckyvickyauction/LuckyVickyAuctionApplication.java
@@ -1,10 +1,7 @@
 package com.wootecam.luckyvickyauction;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class LuckyVickyAuctionApplication {
@@ -13,16 +10,4 @@ public class LuckyVickyAuctionApplication {
         SpringApplication.run(LuckyVickyAuctionApplication.class, args);
     }
 
-
-    @Bean
-    public CommandLineRunner printEnvVariables(Environment env) {
-        return args -> {
-            System.out.println("DB_ENDPOINT: " + env.getProperty("DB_ENDPOINT"));
-            System.out.println("DB_PORT: " + env.getProperty("DB_PORT"));
-            System.out.println("DB_NAME: " + env.getProperty("DB_NAME"));
-            System.out.println("DB_USERNAME: " + env.getProperty("DB_USERNAME"));
-            System.out.println("REDIS_HOST: " + env.getProperty("REDIS_HOST"));
-            System.out.println("REDIS_PORT: " + env.getProperty("REDIS_PORT"));
-        };
-    }
 }
