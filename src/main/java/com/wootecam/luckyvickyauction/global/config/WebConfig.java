ebdd0429fd7a27de8b982e937d5b0f29e51aeda6
Basak <chgy2131@naver.com>
2024-08-25 12:10:08 +0900

    feat: add CORS origins for localhost:3001 and front.dbsg.co.kr:3000
    following origins:
      - http://localhost:3000
      - http://localhost:3001
      - http://front.dbsg.co.kr:3000

--- src/main/java/com/wootecam/luckyvickyauction/global/config/WebConfig.java
+++ src/main/java/com/wootecam/luckyvickyauction/global/config/WebConfig.java
@@ -22,7 +22,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://localhost:3001",
+                        "http://front.dbsg.co.kr:3000"
+                )
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .allowedMethods("GET", "HEAD", "POST", "DELETE", "TRACE", "OPTIONS", "PATCH", "PUT");
