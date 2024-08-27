703152fb3cbc05669d1b1bcac29c46a3c1ab3034
yudonggeun <ydong98@gmail.com>
2024-08-27 15:32:08 +0900

    refactor: api 패키지 분리

--- api/src/main/java/com/wootecam/api/ApiApplication.java
+++ api/src/main/java/com/wootecam/api/ApiApplication.java
@@ -3,7 +3,7 @@ package com.wootecam.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.wootecam.core", "com.wootecam.api"})
 public class ApiApplication {
 
     public static void main(String[] args) {
