c1bcdcdd2f89515ee80d61a2a96dd9698742d322
yudonggeun <ydong98@gmail.com>
2024-08-29 01:22:26 +0900

    feat: 입찰(구매) 실패시 실패 거래 내역 생성 기능 구현

--- consumer/src/main/java/com/wootecam/luckyvickyauction/service/MessageRouterService.java
+++ consumer/src/main/java/com/wootecam/luckyvickyauction/service/MessageRouterService.java
@@ -3,9 +3,12 @@ package com.wootecam.luckyvickyauction.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.wootecam.luckyvickyauction.domain.entity.type.ReceiptStatus;
 import com.wootecam.luckyvickyauction.dto.auction.message.AuctionPurchaseRequestMessage;
 import com.wootecam.luckyvickyauction.dto.auction.message.AuctionRefundRequestMessage;
+import com.wootecam.luckyvickyauction.dto.receipt.command.CreateReceiptCommand;
 import com.wootecam.luckyvickyauction.service.auctioneer.Auctioneer;
+import com.wootecam.luckyvickyauction.service.receipt.ReceiptService;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.stream.MapRecord;
@@ -17,9 +20,11 @@ public class MessageRouterService {
 
     private final Auctioneer auctioneer;
     private final ObjectMapper objectMapper;
+    private final ReceiptService receiptService;
 
-    public MessageRouterService(Auctioneer auctioneer) {
+    public MessageRouterService(Auctioneer auctioneer, ReceiptService receiptService) {
         this.auctioneer = auctioneer;
+        this.receiptService = receiptService;
 
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
@@ -40,8 +45,7 @@ public class MessageRouterService {
             try {
                 switch (messageType) {
                     case "purchase":
-                        auctioneer.process(objectMapper.readValue((String) message.get(messageType),
-                                AuctionPurchaseRequestMessage.class), postProcess);
+                        purchase(messageType, message, postProcess);
                         break;
                     case "refund":
                         auctioneer.refund(objectMapper.readValue((String) message.get(messageType),
@@ -53,6 +57,26 @@ public class MessageRouterService {
             } catch (JsonProcessingException e) {
                 throw new RuntimeException(e);
             }
+        }
+    }
+
+    private void purchase(String messageType, Map<Object, Object> message, Runnable postProcess)
+            throws JsonProcessingException {
+        var objectMessage = objectMapper.readValue((String) message.get(messageType),
+                AuctionPurchaseRequestMessage.class);
+        try {
+            auctioneer.process(objectMessage, postProcess);
+        } catch (RuntimeException e) {
+            // 경매 입찰(구매) 요청이 실패하면 구매 실패 거래 내역을 생성한다.
+            var command = CreateReceiptCommand.builder()
+                    .requestId(objectMessage.getRequestId())
+                    .quantity(objectMessage.getQuantity())
+                    .price(objectMessage.getPrice())
+                    .auctionId(objectMessage.getAuctionId())
+                    .buyerId(objectMessage.getBuyerId())
+                    .status(ReceiptStatus.FAILED)
+                    .build();
+            receiptService.createReceipt(command, postProcess);
         }
     }
 }
