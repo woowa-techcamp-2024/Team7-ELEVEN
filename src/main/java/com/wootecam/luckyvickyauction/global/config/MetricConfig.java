5bd754c89cb64e41621bf6310030b83252f5248c
유동근 <67232422+yudonggeun@users.noreply.github.com>
2024-08-29 12:20:15 +0900

    feat: dev Auctioneer 입찰 로직 실행 시간 측정 메트릭 적용 (#334)
    * feat: BasicAuctioneer timed 메트릭 적용
    
    * chore: 커스텀 메트릭 대시보드 적용

--- src/main/java/com/wootecam/luckyvickyauction/global/config/MetricConfig.java
+++ src/main/java/com/wootecam/luckyvickyauction/global/config/MetricConfig.java
@@ -1,5 +1,6 @@
 package com.wootecam.luckyvickyauction.global.config;
 
+import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -11,5 +12,10 @@ public class MetricConfig {
     @Bean
     MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
         return registry -> registry.config().commonTags("application", "lucky-vicky-auction");
+    }
+
+    @Bean
+    public TimedAspect timedAspect(MeterRegistry registry) {
+        return new TimedAspect(registry);
     }
 }
