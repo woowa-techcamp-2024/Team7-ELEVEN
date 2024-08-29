423996bec2691cd422215b1f0f5533911d451700
이호석 <66772624+HiiWee@users.noreply.github.com>
2024-08-29 13:24:15 +0900

    feat: 2개의 EC2로 자동 배포하도록 스크립트를 변경합니다. (#338)
    * remove: 사용하지 않는 consumer 패키지 삭제
    
    * chore: 2개의 EC2로 배포하도록 변경
    
    * feat: 영수증 생성 날짜 auditing 기능 추가 및 내림차순 정렬
    
    * fix: 잘못된 어노테이션 수정 (CreatedBy -> CreatedDate)

--- src/main/java/com/wootecam/luckyvickyauction/core/payment/entity/ReceiptEntity.java
+++ src/main/java/com/wootecam/luckyvickyauction/core/payment/entity/ReceiptEntity.java
@@ -2,6 +2,7 @@ package com.wootecam.luckyvickyauction.core.payment.entity;
 
 import com.wootecam.luckyvickyauction.core.payment.domain.ReceiptStatus;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
@@ -12,10 +13,17 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Entity
 @Table(name = "RECEIPT")
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ReceiptEntity {
     @Id
@@ -28,7 +36,9 @@ public class ReceiptEntity {
     private long auctionId;
     private Long sellerId;
     private Long buyerId;
+    @CreatedDate
     private LocalDateTime createdAt;
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 
     @Builder
